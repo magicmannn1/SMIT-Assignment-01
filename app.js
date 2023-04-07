237e15041f598c4fe74465f272c3463322026d5a
magicmannn1 <130100825+magicmannn1@users.noreply.github.com>
2023-04-07 19:32:44 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,69 @@
+// 1. Write a JavaScript program that accepts two numbers in two prompts and displays the larger one in the console.
+
+// let Number1 = +prompt("Enter First Number");
+// let Number2 = +prompt("Enter Second Number");
+// if (Number1 > Number2) {console.log(Number1)} else {console.log(Number2)};
+
+
+// 2. Write a JavaScript conditional statement to find the sign of a number. Display an alert box with the specified sign
+
+// let Number = prompt("Enter The Number");
+// if  (data = Number.includes("-")) {console.log("The Sign is -")}
+// else {console.log("The Sign Is +")};
+
+
+// 3. Write a JavaScript program that accepts five numbers in five prompts and displays the larger one in the console
+
+// let Num1 = +prompt("Enter First Number");
+// let Num2 = +prompt("Enter Second Number");
+// let Num3 = +prompt("Enter Third Number");
+// let Num4 = +prompt("Enter Fourth Number");
+// let Num5 = +prompt("Enter Fifth Number");
+
+// if (Num1>Num2 && Num1>Num3 && Num1>Num4 && Num1>Num5)
+// {console.log(Num1);}
+// else if (Num2>Num1 && Num2>Num3 && Num2>Num4 && Num2>Num5)
+// {console.log(Num2);}
+// else if (Num3>Num1 && Num3>Num2 && Num3>Num4 && Num3>Num5)
+// {console.log(Num3);}
+// else if (Num4>Num1 && Num4>Num2 && Num4>Num3 && Num4>Num5)
+// {console.log(Num4);}
+// else
+// {console.log(Num5);}
+
+
+// 4. Write a JavaScript for loop that will iterate from 0 to 15. For each iteration, it will check if the current number is odd or even, and display a message to the screen.
+
+// for (let i=0; i<=10; i++) 
+// { if (i === 0) {console.log(i +  " is even");}
+//   else if (i % 2 === 0) {console.log(i+ " is even");}
+//   else  {console.log(i + " is odd");} }  
+
+
+// 5.Write a JavaScript program which computes the average marks of the following students Then, this average is used to determine the corresponding grade.
+
+// let Marks= +prompt("Enter Your Acquired Marks")
+// if (Marks => 90) {console.log("You Got A Grade!")}
+// else if (Marks =>80) {console.log("You Got B Grade!")}
+// else if (Marks => 70) {console.log("You Got C Grade!")}
+// else if (Marks => 60) {console.log("You Got D Grade!")}
+// else {console.log("You Got F Grade!")}
+
+
+//6. Write a JavaScript program which iterates the integers from 1 to 100. But for multiples of three print "Fizz" instead of the number and for the multiples of fiveprint "Buzz". For numbers which are multiples of both three and five print"FizzBuzz".
+
+// for ( var i = 1; i <= 100; i++ )
+// { if ( i%3 === 0 ) {console.log( i + " Fizz" );}
+//   else if ( i%5 === 0 ) {console.log(i+ " Buzz" );}
+//   else if ( i%3 === 0 && i%5 === 0 ) {console.log(i+ " FizzBuzz" );}
+//   else {console.log(i);} }
+
+
+//7. Write a JavaScript program to construct the following pattern, using a nested for loop.
+
+// let x,y,chr;
+// for(i=1; i <=6; i++)
+// { for (x=1; x < i; x++)
+//  {chr = chr + ("*");}
+//  console.log(chr);
+//  chr = " ";}
